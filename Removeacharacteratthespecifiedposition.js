2a886d7b1df12b0e9909f0638f8f6e7b9031b412
wpxmunna <wpxmunna@gmail.com>
2023-06-17 13:09:30 +0600

    adding py and remove char problem solve file added

--- Removeacharacteratthespecifiedposition.js
+++ Removeacharacteratthespecifiedposition.js
@@ -1,6 +1,8 @@
 function removeStr(charPos, str){
     var str1 = str.substring(0,charPos);
+    console.log(str1);
     var str2 = str.substring(charPos + 1, str.length);
+    console.log(str2);
     return str1 + str2;
 }
 console.log(removeStr(0, 'Python'));
